516c7bcf6b9a2b62d395bb8b8b95089ffc9148da
rzwen <1155141547@link.cuhk.edu.hk>
2022-11-23 21:19:04 +0800

    11-23-5

--- js/datapresent.js
+++ js/datapresent.js
@@ -640,7 +640,7 @@ function getDatasetList(){
     return new Promise((resolve,reject)=>{
         setTimeout(function(){
             resolve()
-        },100);
+        },0);
     })
 }
 
@@ -659,7 +659,7 @@ function getIndexes(){
     return new Promise((resolve,reject)=>{
         setTimeout(function(){
             resolve()
-        },100);
+        },0);
     })
 }
 
@@ -717,7 +717,7 @@ function getMtCSV(){
     return new Promise((resolve,reject)=>{
         setTimeout(function(){
             resolve()
-        },100);
+        },0);
     })
 }
 
